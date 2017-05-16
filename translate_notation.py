76d0b92a1de6a79685f4458375ebf24f61d55e87
Nils Wentzell <nils.wentzell@gmail.com>
2017-05-16 11:35:11 +0200

    changed to work with matsubara's directly

--- translate_notation.py
+++ translate_notation.py
@@ -14,103 +14,68 @@ fromferm ={ };
 
 #-------- Purely fermionic-> Identity-------------------------
 
-toferm['Ferm'] = lambda (i,j,k):(i,j,k)
+toferm['FERM'] = lambda (w1_in,w2_in,w1_out):(w1_in,w2_in,w1_out)
 
-fromferm['Ferm'] = lambda (i,j,k):(i,j,k)
+fromferm['FERM'] = lambda (w1_in,w2_in,w1_out):(w1_in,w2_in,w1_out)
 
-#-------- PP shifted-------------------------
+#-------- TRIQS -------------------------
 
-toferm['PP_shift'] = lambda (i,j,k):(j+myceil_div2(i),-j+myfloor_div2(i)-1,k+myceil_div2(i))
+toferm['TRIQS'] = lambda (w1_in, w2_in, w1_out):(w1_in, w1_out, w2_in)
 
-fromferm['PP_shift'] = lambda (i,j,k):(i+j+1,i-myceil_div2(i+j+1),k-myceil_div2(i+j+1))
+fromferm['TRIQS'] = lambda (w1_in, w1_out, w2_in):(w1_in, w2_in, w1_out)
 
-#-------- PP shifted paper-------------------------
+#-------- POMEROL -------------------------
 
-toferm['PP_shiftpap'] = lambda (i,j,k):(j+myceil_div2(i),-j+myfloor_div2(i)-1,-k-myfloor_div2(i)-1)
+toferm['POM'] = lambda (w1_out, w2_out, w2_in):(w1_out + w2_out - w2_in, w2_in, w1_out)
 
-fromferm['PP_shiftpap'] = lambda (i,j,k):(i+j+1,i-myceil_div2(i+j+1),-k-1+myfloor_div2(i+j+1))
+fromferm['POM'] = lambda (w1_in, w2_in, w1_out):(w1_out, w1_in + w2_in - w1_out, w2_in)
+
+#-------- PP ( Vertex paper, only outgoing legs flipped ) -------------------------
+
+toferm['PP'] = lambda (W, w_in, w_out):(w_in, W - w_in, w_out)
+
+fromferm['PP'] = lambda (w1_in, w2_in, w1_out):(w1_in + w2_in, w1_in, w1_out)
+
+#-------- PH ( Vertex paper ) -------------------------
+
+toferm['PH'] = lambda (W, w_in, w_out):(w_in, w_out + W, w_in + W)
+
+fromferm['PH'] = lambda (w1_in, w2_in, w1_out):(w1_out - w1_in, w1_in, w2_in - (w1_out - w1_in))
+
+#-------- XPH ( Vertex paper ) -------------------------
+
+toferm['XPH'] = lambda (W, w_in, w_out):(w_in, w_out + W, w_out)
+
+fromferm['XPH'] = lambda (w1_in, w2_in, w1_out):(w2_in - w1_out, w1_in, w2_in - (w2_in - w1_out))
+
+# ===== ADJUST 
+
+# #-------- PP shifted-------------------------
+
+# toferm['PP_shift'] = lambda (i,j,k):(j+ceil_div2(i),-j+floor_div2(i)-1,k+ceil_div2(i))
+
+# fromferm['PP_shift'] = lambda (i,j,k):(i+j+1,i-ceil_div2(i+j+1),k-ceil_div2(i+j+1))
 
 
-#-------- PH shifted-------------------------
+# #-------- PH shifted-------------------------
 
-toferm['PH_shift'] = lambda (i,j,k):(j-myfloor_div2(i),k+myceil_div2(i),j+myceil_div2(i))
+# toferm['PH_shift'] = lambda (i,j,k):(j-floor_div2(i),k+ceil_div2(i),j+ceil_div2(i))
 
-fromferm['PH_shift'] = lambda (i,j,k):(k-i,i+myfloor_div2(k-i),j-myceil_div2(k-i))
-
-
-#-------- XPH shifted-------------------------
-
-toferm['XPH_shift'] = lambda (i,j,k):(j-myfloor_div2(i),k+myceil_div2(i), k-myfloor_div2(i))
-
-fromferm['XPH_shift'] = lambda (i,j,k):(j-k,i+myfloor_div2(j-k),j-myceil_div2(j-k))
+# fromferm['PH_shift'] = lambda (i,j,k):(k-i,i+floor_div2(k-i),j-ceil_div2(k-i))
 
 
-#-------- PP georg -> time reversal symmetry assumed-------------------------
+# #-------- XPH shifted-------------------------
 
-toferm['PP_georg'] = lambda (i,j,k):(i-k-1,k,j)
+# toferm['XPH_shift'] = lambda (i,j,k):(j-floor_div2(i),k+ceil_div2(i), k-floor_div2(i))
 
-fromferm['PP_georg'] = lambda (i,j,k):(i+j+1,k,j)
+# fromferm['XPH_shift'] = lambda (i,j,k):(j-k,i+floor_div2(j-k),j-ceil_div2(j-k))
 
-
-#-------- PH georg -> time reversal symmetry assumed-------------------------
-
-toferm['PH_georg'] = lambda (i,j,k):(i+k,j,k)
-
-fromferm['PH_georg'] = lambda (i,j,k):(i-k,j,k)
 
 #--------------- TRANSLATION FUNCTION -----------------------------
 
 def translate( notout, notin ):
     return lambda (i,j,k): fromferm[notout]((toferm[notin]((i,j,k))))
 
+
 #------------------- Define useful objects--------------------------
 
-PPtoPH = translate('PH_shift','PP_shift')
-PHtoPP = translate('PP_shift','PH_shift')
-
-PPtoXPH = translate('XPH_shift','PP_shift')
-XPHtoPP = translate('PP_shift', 'XPH_shift')
-
-PPtoPPpap = translate('PP_shiftpap','PP_shift')
-PPpaptoPP = translate('PP_shift', 'PP_shiftpap')
-
-PHtoXPH = translate('XPH_shift','PH_shift')
-XPHtoPH = translate('PH_shift','XPH_shift')
-
-PPtoPPg = translate('PP_georg','PP_shift')
-PPgtoPP = translate('PP_shift','PP_georg')
-
-PPpaptoPH = translate('PH_shift','PP_shiftpap')
-PHtoPPpap = translate('PP_shiftpap','PH_shift')
-
-PPpaptoXPH = translate('XPH_shift','PP_shiftpap')
-XPHtoPPpap = translate('PP_shiftpap', 'XPH_shift')
-
-PHtoPHg = translate('PH_georg','PH_shift')
-PHgtoPH = translate('PH_shift','PH_georg')
-
-PFtoPH = translate('PH_shift','Ferm')
-PHtoPF = translate('Ferm','PH_shift')
-
-PFtoPP = translate('PP_shift','Ferm')
-PPtoPF = translate('Ferm','PP_shift')
-
-PFtoXPH = translate('XPH_shift','Ferm')
-XPHtoPF = translate('Ferm','XPH_shift')
-
-PFtoPHg = translate('PH_georg','Ferm')
-PHgtoPF = translate('Ferm','PH_georg')
-
-PFtoPPg = translate('PP_georg','Ferm')
-PPgtoPF = translate('Ferm','PP_georg')
-
-PHtoPH = translate('PH_shift','PH_shift')
-XPHtoXPH = translate('XPH_shift','XPH_shift')
-PPtoPP = translate('PP_shift','PP_shift')
-PPgtoPPg = translate('PP_georg','PP_georg')
-PHgtoPHg = translate('PH_georg','PH_georg')
-
-#print PPgtoPP((0,-114,0))
-#print PPgtoPPg((0,0,0))
-#print PHgtoPH((39,40,40)), PPgtoPP((0,20,40))
-#print PPtoPP((1,2,3)), PHtoPH((1,2,3)), XPHtoXPH((1,2,3))
